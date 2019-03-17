de4c6a044acd09d232bff66ba7f95b5f4dc26f6d
Hanna Andersson <hannaandersson010@gmail.com>
2019-03-17 15:32:36 +0100

    added scroll to My-work

--- src/views/MyWork/MyWork.js
+++ src/views/MyWork/MyWork.js
@@ -1,15 +1,27 @@
-import React from "react";
+import React, { Component } from "react";
 import "./MyWork.scss";
 import Cover from "../../components/Cover/Cover";
 import ImagesSection from "../../components/ImageSection/ImageSection";
+import { animateScroll as scroll } from "react-scroll";
 
-const MyWork = () => {
-  return (
-    <div className="myWork">
-      <Cover />
-      <ImagesSection />
-    </div>
-  );
-};
+class MyWork extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
 
+  componentDidMount = () => {
+    scroll.scrollToTop({
+      duration: 0
+    });
+  };
+  render() {
+    return (
+      <div className="myWork">
+        <Cover />
+        <ImagesSection />
+      </div>
+    );
+  }
+}
 export default MyWork;
